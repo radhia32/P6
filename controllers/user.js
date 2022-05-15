c3eab2f703696b044857b4bd090eeb53c20f4abc
Radhia El Mabrouk <radia32@hotmail.com>
2022-05-15 13:10:21 +0200

    p6

--- controllers/user.js
+++
@@ -1,5 +0,0 @@
-const User = require('./models/user.js')
-const jwt = require('jsonwebtoken')
-const bcrypt= require( 'bcryptjs');
-re('jsonwebtoken')
-const cryptojs = require('crypto-js');
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const sauceRoutes =require('./sauce');
-
- (app) => {
-  app.use('/api/sauces', sauceRoutes);
-};
-module.exports = router;
